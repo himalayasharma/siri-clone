90bb349cdcc90791e8e79e27b9d50c8d020a8736
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-29 17:09:13 +0530

    Add code to begin transcription job

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ from record_mic import record_mic
 from speech_to_text import speechToText
 
 def main():
+    # 1. RECORD AND SAVE AUDIO
     audio_file_path = record_mic()
+    # audio_file_path = "data/audio-recording/2022-08-29-15:15:14.353041-recording.wav"
+    # 2. UPLOAD AUDIO FILE TO ASSEMBLY AI
     obj = speechToText()
-    response = obj.upload_audio(audio_file_path)
-    print(response)
+    # Audio url in json is returned
+    audio_url = obj.upload_audio(audio_file_path)
+    # 3. SUBMIT UPLOADED FILE FOR TRANSCRIPTION
+    transcription_job_id = obj.submit_for_transcription(audio_url=audio_url)
+
 
 if __name__ == "__main__":
     main()
