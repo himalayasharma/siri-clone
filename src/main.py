0eb78029cdea7a8cde9f8bf90473a2c72f9ab236
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-29 20:06:01 +0530

    Loop to get question prompts

--- src/main.py
+++ src/main.py
@@ -5,25 +5,26 @@ from openai_response import get_answer
 RECORD_DURATION=4
 
 def main():
-    # 1. RECORD AND SAVE AUDIO
-    audio_file_path = record_mic(record_duration=RECORD_DURATION)
-    # audio_file_path = "data/audio-recording/2022-08-29-15:15:14.353041-recording.wav"
-    # 2. UPLOAD AUDIO FILE TO ASSEMBLY AI
-    obj = speechToText()
-    # Audio url in json is returned
-    audio_url = obj.upload_audio(audio_file_path)
-    # 3. SUBMIT UPLOADED FILE FOR TRANSCRIPTION
-    transcription_job_id = obj.submit_for_transcription(audio_url=audio_url)
-    # 4. GET TRANSCRIPTION
-    question = obj.get_transcription(transcription_job_id=transcription_job_id)
-    print(40*"*")
-    print(f"ME: {question}")
-    print(40*"*")
-    # 5. SUBMIT QUESTION TO OPEN AI AND GET RESPONSE
-    answer = get_answer(question=question)
-    print(40*"*")
-    print(f"JARVIS: {answer}")
-    print(40*"*")
+    while True:
+        # 1. RECORD AND SAVE AUDIO
+        audio_file_path = record_mic(record_duration=RECORD_DURATION)
+        # audio_file_path = "data/audio-recording/2022-08-29-15:15:14.353041-recording.wav"
+        # 2. UPLOAD AUDIO FILE TO ASSEMBLY AI
+        obj = speechToText()
+        # Audio url in json is returned
+        audio_url = obj.upload_audio(audio_file_path)
+        # 3. SUBMIT UPLOADED FILE FOR TRANSCRIPTION
+        transcription_job_id = obj.submit_for_transcription(audio_url=audio_url)
+        # 4. GET TRANSCRIPTION
+        question = obj.get_transcription(transcription_job_id=transcription_job_id)
+        print(40*"*")
+        print(f"ME: {question}")
+        print(40*"*")
+        # 5. SUBMIT QUESTION TO OPEN AI AND GET RESPONSE
+        answer = get_answer(question=question)
+        print(40*"*")
+        print(f"JARVIS: {answer}")
+        print(40*"*")
 
 if __name__ == "__main__":
     main()
